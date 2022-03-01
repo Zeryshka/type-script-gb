7e4138e562756330133d0da9dc365c075bb9203e
Zeryshka <maxim98sorokin@gmail.com>
2022-03-01 15:44:35 +0300

    Очищен мусор

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import { cloneDate, addDays, FlatRentSdk } from './flat-rent-sdk/flat-rent-sdk'
-
-cloneDate(new Date());
-addDays(new Date(), 5);
-
-const flat = new FlatRentSdk();
-
-flat.get(1337);
